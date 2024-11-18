892c0cfa3b468a0df9d641cb16202774fcad5594
Dawit Mellese <dawitm777@gmail.com>
2024-11-18 15:34:34 +0300

    bug fix

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,20 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Trasactions from "./pages/Trasactions";
+import Subscriptions from "./pages/Subscriptions";
+import Setting from "./pages/Setting";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/transactions" element={<Trasactions />} />
+        <Route path="/subscriptions" element={<Subscriptions />} />
+        <Route path="/settings" element={<Setting />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
